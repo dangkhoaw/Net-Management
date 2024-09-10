4199f35706f18a4644ce402e22e0df4fa071d20d
grayzy2617 <phat058zz@gmail.com>
2024-09-10 08:37:50 +0700

    tổ chức ctdl và thêm vài biến

--- account.h
+++ account.h
@@ -35,11 +35,26 @@ Account::~Account() {}
 string Account::getRole() { return role; }
 
 // class of customer
-class Customer : public Account
+class HistoryNode
+{
+private:
+    string time;
+    HistoryNode *next;
+
+public:
+    HistoryNode();
+    HistoryNode(string time);
+    ~HistoryNode();
+    friend class Customer;
+};
+//
+//
+class Customer : public Account //
 {
 private:
     string phone;
-    string hour;
+    int remainingTime;        // đổi ra giây hết thay thế cho hour
+    HistoryNode *historyHead; // lịch sử dụng máy bằng linklist
 
 public:
     Customer();
@@ -48,13 +63,25 @@ public:
 
     string getPhone();
     string getHour();
-
-    void changepassword();
+    void displayTimeLeft();
+    void addHistory();
+    void changePassword();
 };
 class Staff : public Account
 {
-private:
+private: // sử dụng mảng và bảng băm
+    Customer customer[21];
+    // bảng băm để lưu thông tin khách hàng ..
 public:
+    Staff();
+    Staff(string username, string password, string role);
+    ~Staff();
+    void viewComputerInfor();
+    void TopUpAccount(int amount);
+    void viewCustomerInfor();
+    void addComputer(int amountofComputer);
+    void ViewRevenue();
+    void lockComputer(Customer &customer);
 };
 
 #endif
